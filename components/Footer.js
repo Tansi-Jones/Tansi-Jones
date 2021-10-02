e8b2e182bb97025df27af1e54fa53202e535b364
Tansi Jones <tansijones@outlook.com>
2021-10-02 12:49:10 +0300

    Cleaning portfolio, restarting my portfolio design

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <>
-      <footer className="bg-gray-800 p-5 italic text-center text-white text-lg">
-        <h1>Made with &hearts; by Tansi Jones &copy; 2021</h1>
-      </footer>
-    </>
-  );
-}
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,11 @@
-import Work from "../components/Work";
-import About from "../components/About";
-import Header from "../components/Header";
 import MetaHead from "../components/MetaHead";
-import Navbar from "../components/Navbar";
-import Skill from "../components/Skill";
-import Footer from "../components/Footer";
-
-import ProjectData from "../components/projectData";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className=" relative overflow-hidden" id="home">
+    <>
       <MetaHead />
-
-      {/*  */}
-      <section
-        className="fixed py-5  w-full z-50 bg-opacBlack backdrop-blur-md"
-        id="stickyNav"
-      >
-        <Navbar />
-      </section>
-
-      <Header />
-
-      {/*  */}
-      <section className="w-[80%] mx-auto ">
-        <About />
-      </section>
-
-      {/*  */}
-      <section className="w-[80%] mx-auto mt-32" id="skills">
-        <p className="text-gray-400 mb-4 md:text-lg">// TECHNOLOGIES</p>
-        <Skill />
-      </section>
-
-      {/*  */}
-      <section className="w-[80%] mx-auto mt-32" id="work">
-        <p className="text-gray-400 mb-4 md:text-lg">// MY WORK</p>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-x-10 gap-y-14 mt-16 mb-20">
-          {ProjectData.map(
-            ({ img, title, description, markers, url }, index) => (
-              <Work
-                id={index}
-                imgSrc={img}
-                heading={title}
-                desc={description}
-                mark={markers}
-                link={url}
-              />
-            )
-          )}
-        </div>
-      </section>
-
-      {/*  */}
-      <section>
-        <Footer />
-      </section>
-    </main>
+      <Image src="/comingsoon.svg" layout="fill" className=" object-cover" />
+    </>
   );
 }
